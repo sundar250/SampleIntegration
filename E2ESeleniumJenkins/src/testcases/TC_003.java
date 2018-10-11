a6766a9d54d29217ee9b06809d499cd4452f01ce
IBM <IBM@IBM829-PC0GJ030>
2018-10-11 13:54:15 +0530

    Created Automation project

---
+++ E2ESeleniumJenkins/src/testcases/TC_003.java
@@ -0,0 +1,23 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_003 {
+
+	@Test
+	public void testcase3() {
+		System.setProperty("webdriver.chrome.driver",
+				"./Drivers/chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.facebook.com");
+
+		driver.findElement(By.id("email")).sendKeys("user1");
+		driver.findElement(By.id("pass")).sendKeys("pass1");
+	}
+
+}
